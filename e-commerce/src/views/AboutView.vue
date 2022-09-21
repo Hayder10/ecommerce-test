5c43874007c31545e259c0da6095ff1808a1f801
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-09-21 17:56:16 -0300

    pruebas vue

--- e-commerce/src/views/AboutView.vue
+++ e-commerce/src/views/AboutView.vue
@@ -1,6 +1,26 @@
+<script setup>
+  const contador2 = {
+    data() {
+      return {
+        contador : 0
+      }
+    },
+    mounted() {
+      setInterval(() => {
+        this.contador ++;
+      }, 1000)
+    }
+  }
+  var mountedApp = Vue.createApp(contador2).mount('#app')
+</script>
+
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div id="app2">
+      Contador: {{contador}}
+    </div>
   </div>
 </template>
 
@@ -11,5 +31,9 @@
     display: flex;
     align-items: center;
   }
+}
+#app2 {
+  font-size: 40px;
+  margin-left: 10px;
 }
 </style>
